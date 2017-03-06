e7ffd722f86762e3880c0260667ce8381ba1f73f
madmarks <bratmiro@gmail.com>
2017-03-06 13:31:13 +0200

    remove Dagger from Login activity (temporarily)

--- app/src/main/java/com/childlocator/firebase/ui/login/LoginActivity.java
+++ app/src/main/java/com/childlocator/firebase/ui/login/LoginActivity.java
@@ -1,121 +1,127 @@
 package com.childlocator.firebase.ui.login;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.childlocator.firebase.App;
 import com.childlocator.firebase.R;
-import com.childlocator.firebase.base.BaseActivity;
-import com.childlocator.firebase.data.model.User;
+import com.childlocator.firebase.data.Constants;
 import com.childlocator.firebase.ui.children.ChildrenActivity;
-
-import javax.inject.Inject;
+import com.childlocator.firebase.ui.register.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
 
-public class LoginActivity extends BaseActivity {
-
+public class LoginActivity extends AppCompatActivity {
   @Bind(R.id.etEmail)
   EditText etEmail;
-
   @Bind(R.id.etPw)
   EditText etPw;
-
   @Bind(R.id.btnLogin)
   Button btnLogin;
+  @Bind(R.id.btnRegister)
+  Button btnRegister;
 
-  @Bind(R.id.pbLoading)
-  ProgressBar pbLoading;
-
-  @Inject
-  LoginScreenPresenter presenter;
-  @Inject
-  AlertDialog.Builder addAlertDialog;
+  private FirebaseAuth mAuth;
+  private FirebaseAuth.AuthStateListener mAuthListener;
+  private static final String LOG_TAG = "LoginActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
     ButterKnife.bind(this);
+    EventBus.getDefault().register(this);
+    mAuth = FirebaseAuth.getInstance();
+    mAuthListener = new FirebaseAuth.AuthStateListener() {
+      @Override
+      public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+          // User is signed in
+          Log.d(LOG_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+        } else {
+          // User is signed out
+          Log.d(LOG_TAG, "onAuthStateChanged:signed_out");
+        }
+
+        // ...
+        if (user != null) {
+          startActivity(new Intent(LoginActivity.this, ChildrenActivity.class));
+          finish();
+        }
+      }
+    };
   }
 
   @Override
-  protected void onResume() {
-    super.onResume();
-    presenter.subscribe();
+  public void onStart() {
+    super.onStart();
+    mAuth.addAuthStateListener(mAuthListener);
   }
 
   @Override
-  protected void onStop() {
+  public void onStop() {
     super.onStop();
-    presenter.unsubscribe();
+    if (mAuthListener != null) {
+      mAuth.removeAuthStateListener(mAuthListener);
+    }
   }
 
   @Override
-  protected void setupActivityComponent() {
-    App.get(this).getAppComponent()
-            .plus(new LoginScreenModule(this))
-            .inject(this);
+  protected void onDestroy() {
+    EventBus.getDefault().unregister(this);
+    super.onDestroy();
+  }
+
+  public void onEvent(String event) {
+    if (event.equals(Constants.KEY_CLOSE)) {
+      LoginActivity.this.finish();
+    }
   }
 
   @OnClick(R.id.btnLogin)
-  public void onBtnLogin() {
+  public void btnLogin() {
     String email = etEmail.getText().toString();
     String password = etPw.getText().toString();
+    if (email.isEmpty() || password.isEmpty()) {
 
-    presenter.loginWithEmail(email, password);
+    } else {
+      mAuth.signInWithEmailAndPassword(email, password)
+              .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                  Log.d(LOG_TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                  // If sign in fails, display a message to the user. If sign in succeeds
+                  // the auth state listener will be notified and logic to handle the
+                  // signed in user can be handled in the listener.
+                  if (!task.isSuccessful()) {
+                    Log.w(LOG_TAG, "signInWithEmail", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT)
+                            .show();
+                  }
+
+                  // ...
+                }
+              });
+    }
   }
 
-  public void showLoginFail() {
-    Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
-  }
-
-  public void showLoginSuccess(User user) {
-    Intent intent = new Intent(LoginActivity.this, ChildrenActivity.class);
-    startActivity(intent);
-    finish();
-  }
-
-  public void showLoading(boolean loading) {
-    pbLoading.setVisibility(loading ? View.VISIBLE : View.GONE);
-  }
-
-  public void showInsertUsername(final User user) {
-
-    addAlertDialog.setTitle("Insert your name");
-    addAlertDialog.setMessage("(be sure to enter)");
-
-    final EditText etUsername = new EditText(this);
-    etUsername.setSingleLine();
-    addAlertDialog.setView(etUsername);
-
-    addAlertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-      public void onClick(DialogInterface dialog, int whichButton) {
-        String username = etUsername.getText().toString();
-        dialog.dismiss();
-        presenter.createUser(user, username);
-      }
-    });
-
-    addAlertDialog.show();
-  }
-
-  public void showExistUsername(User user, String username) {
-    Toast.makeText(this, "Exist username" + username, Toast.LENGTH_LONG).show();
-    showInsertUsername(user);
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
+  @OnClick(R.id.btnRegister)
+  public void setBtnRegister() {
+    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
   }
 }
